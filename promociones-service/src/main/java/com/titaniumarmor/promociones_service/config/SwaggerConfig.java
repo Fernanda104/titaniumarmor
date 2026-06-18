57283ea96731914a963d85899aae861951f60838
Cristopher Guerra <cr.guerrav@duocuc.cl>
2026-06-18 16:53:18 -0400

    Gateway ahora si 1234

--- promociones-service/src/main/java/com/titaniumarmor/promociones_service/config/SwaggerConfig.java
+++ promociones-service/src/main/java/com/titaniumarmor/promociones_service/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.titaniumarmor.promociones_service.controller;
+package com.titaniumarmor.promociones_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
