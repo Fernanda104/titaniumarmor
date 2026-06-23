fc489620c0fafea06af85f7b665241c2bd403a0b
Cristopher Guerra <cr.guerrav@duocuc.cl>
2026-06-23 16:26:05 -0400

    HateOas en Usuarios

--- promociones-service/src/main/java/com/titaniumarmor/promociones_service/config/SwaggerConfig.java
+++ promociones-service/src/main/java/com/titaniumarmor/promociones_service/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI(){
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Promociones de TitaiumArmor")
+                        .title("API Promociones de TitaniumArmor")
                         .version("1.0")
                         .description("Documentación de la API para el sistema de promociones de la tienda TitaniunArmor")
                     );
---
+++ usuarios-service/src/main/java/com/titaniumarmor/usuarios_service/assemblers/UsuarioModelAssembler.java
@@ -0,0 +1,24 @@
+package com.titaniumarmor.usuarios_service.assemblers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.titaniumarmor.usuarios_service.controller.UsuarioControllerV2;
+import com.titaniumarmor.usuarios_service.model.Usuario;
+
+@Component
+public class UsuarioModelAssembler implements RepresentationModelAssembler<Usuario, EntityModel<Usuario>>{
+
+
+   
+    @Override
+    public EntityModel<Usuario> toModel(Usuario usuario) {
+        return EntityModel.of(usuario,
+                linkTo(methodOn(UsuarioControllerV2.class).obtenerUsuario(usuario.getId())).withSelfRel(),
+                linkTo(methodOn(UsuarioControllerV2.class).listarUsuarios()).withRel("usuarios"));  
+            }
+}
